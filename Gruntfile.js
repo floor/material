0931b7c16beac9eef7cad06415ba064643420cd1
Jerome Vial <jerome.vial@gmail.com>
2015-11-09 04:48:28 +0100

    register browserfiy task

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,5 +3,6 @@ module.exports = function(grunt) {
 	require('./tasks/watch.js')(grunt);
 	require('./tasks/less.js')(grunt);
 	require('./tasks/deploy.js')(grunt);
+	require('./tasks/browserify.js')(grunt);
 
 };
