ddf95abf6528f9b724ad78e71559ac7de98fd58e
Jerome Vial <jerome.vial@gmail.com>
2015-11-10 03:29:18 +0100

    require plato grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,5 +4,6 @@ module.exports = function(grunt) {
 	require('./tasks/less.js')(grunt);
 	require('./tasks/deploy.js')(grunt);
 	require('./tasks/browserify.js')(grunt);
+	require('./tasks/plato.js')(grunt);
 
 };
