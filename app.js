4a0fd92f7643ec56a71bf9bd697ccea448908509
Jerome Vial <jerome.vial@gmail.com>
2015-11-11 00:16:16 +0100

    require and use js-logger

--- app.js
+++ app.js
@@ -3,10 +3,21 @@
  * material demo
  * @type {[type]}
  */
+
+var Logger = require('js-logger');
+Logger.useDefaults();
+
 var express = require('express');
 var app = express();
 
-app.use(express.compress());
-app.use(express.static(__dirname + '/dist'));
+var port = 3000;
 
-app.listen(process.env.PORT || 3000);
+app.use(express.compress());
+app.use('/', express.static(__dirname + '/dist'));
+
+Logger.info('dist', __dirname + '/dist');
+
+app.listen(process.env.PORT || port);
+
+Logger.info('app running on port', port);
+
