ca2940d750963e71fd2cf9302726aa16874c40da
Jerome Vial <jerome.vial@gmail.com>
2015-11-26 23:24:16 +0100

    use gulp instead of grunt

--- Gruntfile.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(grunt) {
-
-	require('./tasks/watch.js')(grunt);
-	require('./tasks/less.js')(grunt);
-	require('./tasks/deploy.js')(grunt);
-	require('./tasks/browserify.js')(grunt);
-	require('./tasks/plato.js')(grunt);
-
-};
