35b9239f214a95af76beefdacfb565678d243fce
Jerome Vial <jerome.vial@gmail.com>
2015-11-26 23:24:00 +0100

    add some routes

--- app.js
+++ app.js
@@ -13,7 +13,14 @@ var app = express();
 var port = 3000;
 
 app.use(express.compress());
-app.use('/', express.static(__dirname + '/dist'));
+//app.use('/', express.static(__dirname + '/dist'));
+app.use('/demo', express.static(__dirname + '/dist/demo'));
+app.use('/vendor', express.static(__dirname + '/dist/vendor'));
+app.use('/reports', express.static(__dirname + '/dist/reports'));
+app.use('/vendor', express.static(__dirname + '/dist/report'));
+app.use('/skin', express.static(__dirname + '/dist/skin'));
+app.use('/build', express.static(__dirname + '/build'));
+app.use('/docs', express.static(__dirname + '/docs'));
 
 Logger.info('dist', __dirname + '/dist');
 
