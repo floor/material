5fa81fced2019bf70a66bb284ce08d85f0aad2ec
Jerome Vial <jerome.vial@gmail.com>
2015-12-26 15:05:28 +0100

    add material lib index file

---
+++ index.js
@@ -0,0 +1,15 @@
+
+//automatically generated, do not edit!
+//run `node build` instead
+module.exports = {
+    'component' : require('./lib/component'),
+    'container' : require('./lib/container'),
+    'control' : require('./lib/control'),
+    'button' : require('./lib/control/button'),
+    'field' : require('./lib/control/field'),
+    'switch' : require('./lib/control/switch'),
+    'form' : require('./lib/form'),
+    'layout' : require('./lib/layout'),
+    'list' : require('./lib/list'),
+    'view' : require('./lib/view')
+};
