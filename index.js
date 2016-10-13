3b800a4f6249c87a4f12944cbad9ce915be04a11
Jerome Vial <jerome.vial@gmail.com>
2016-10-13 11:18:51 +0200

    update indentation

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-
 //automatically generated, do not edit!
 //run `node build` instead
 module.exports = {
-    'component' : require('./lib/component'),
-    'container' : require('./lib/container'),
-    'control' : require('./lib/control'),
-    'button' : require('./lib/control/button'),
-    'field' : require('./lib/control/field'),
-    'switch' : require('./lib/control/switch'),
-    'form' : require('./lib/form'),
-    'layout' : require('./lib/layout'),
-    'list' : require('./lib/list'),
-    'view' : require('./lib/view')
+  'component': require('./lib/component'),
+  'container': require('./lib/container'),
+  'control': require('./lib/control'),
+  'button': require('./lib/control/button'),
+  'field': require('./lib/control/field'),
+  'switch': require('./lib/control/switch'),
+  'form': require('./lib/form'),
+  'layout': require('./lib/layout'),
+  'list': require('./lib/list'),
+  'view': require('./lib/view')
 };
