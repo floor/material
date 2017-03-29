0655ca907a05e5fefb565741af78e86d7ca82295
Jerome Vial <jerome.vial@gmail.com>
2017-03-29 17:33:36 +0200

    update src folder in tasks and package

--- tasks/browserify.js
+++ tasks/browserify.js
@@ -10,7 +10,7 @@ var babel = require('babelify');
 
 function compile(watch) {
   // browserify -r material -x mout -x elements/zen -x elements/base -x prime -x mout -x mout/array/every -x mout/array/indexOf -x mout/lang/toString -x mout/array/filter -x mout/array/forEach -x mout/array/map -x mout/array/every -x mout/lang/kindOf -x mout/number/toInt -x mout/time/now -x mout/string/pascalCase -x elements -x moofx 
-  var bundler = watchify(browserify('./lib/index.js', { debug: true })
+  var bundler = watchify(browserify('./src/index.js', { debug: true })
     .ignore('mout')
     .ignore('prime')
     .ignore('elements')
@@ -19,8 +19,10 @@ function compile(watch) {
 
   function rebundle() {
     bundler.bundle()
-      .on('error', function(err) { console.error(err);
-        this.emit('end'); })
+      .on('error', function(err) {
+        console.error(err);
+        this.emit('end');
+      })
       .pipe(source('material.js'))
       .pipe(buffer())
       .pipe(sourcemaps.init({ loadMaps: true }))
@@ -43,8 +45,10 @@ function watch() {
 };
 
 gulp.task('build', function() {
-  return compile(); });
+  return compile();
+});
 gulp.task('watch', function() {
-  return watch(); });
+  return watch();
+});
 
 gulp.task('default', ['watch']);
--- tasks/concat.js
+++ tasks/concat.js
@@ -3,7 +3,7 @@
 var gulp = require('gulp');
 
 gulp.task('concat', function() {
-  return gulp.src('lib/**/*.js')
-    .pipe(concat({ path: 'build/concat.js', stat: { mode: '0666' }}))
+  return gulp.src('src/**/*.js')
+    .pipe(concat({ path: 'build/concat.js', stat: { mode: '0666' } }))
     .pipe(gulp.dest('./build'));
 });
