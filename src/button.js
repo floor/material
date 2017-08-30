17f41cf05d752fa071d2b50a2f407e5da668c95b
Jerome Vial <jerome.vial@gmail.com>
2017-08-30 10:52:42 +0200

    no longer extends Component

--- src/button.js
+++ src/button.js
@@ -1,27 +1,31 @@
 'use strict';
 
 // base class
-import Component from 'component';
+import Text from './text';
+import Image from './image';
+
+import events from './component/events';
+import Emitter from './module/emitter';
+import css from './module/css';
+import merge from './module/merge';
+import insert from './element/insert';
+import bind from './module/bind';
 
 // modules
-import control from 'control';
-import ripple from 'component/ripple';
+import control from './control';
+import ripple from './component/ripple';
 
 var defaults = {
   prefix: 'material',
-  type: 'control', // push, file
   tag: 'button',
-  // components: [{
-  //   tag: 'label.ui-text'
-  // }],
   ripple: {
     duration: '500',
     equation: 'ease-out'
   },
   bind: {
-    'click': ['_onClick', '_showRipple'],
-    'mousedown': ['_onMouseDown'],
-    'mouseup': ['_onMouseUp'],
+    'click': '_onClick',
+    'mousedown': ['_onMouseDown', '_showRipple'],
+    'mouseup': ['_onMouseUp', '_hideRipple'],
     'mouseout': ['_onMouseOut', '_hideRipple']
   }
 };
@@ -40,7 +44,7 @@ var defaults = {
  *   console.log('button press', e);
  * }).insert(document.body);
  */
-class Button extends Component {
+class Button {
 
   /**
    * The init method of the Button class
@@ -48,15 +52,37 @@ class Button extends Component {
    * @private
    * @return {Object} The class instance
    */
+  constructor(options) {
+    this.options = merge(defaults, options);
+
+    this.init();
+    this.build();
+
+    if (this.options.bind) {
+      this.bind(this.options.bind);
+    }
+
+    return this;
+  }
+
+  /**
+   * [init description]
+   * @param  {[type]} options [description]
+   * @return {[type]}         [description]
+   */
   init(options) {
-    super.init(defaults);
+    Object.assign(
+      this,
+      control,
+      ripple,
+      events,
+      Emitter,
+      bind
+    );
 
-    this.options = [this.options, options].reduce(Object.assign, {});
-
-    Object.assign(this, control);
-    Object.assign(this, ripple);
-
-    this.component = {};
+    this.class = this._name = this.constructor.name.toLowerCase();
+    this.name = this.options.name;
+    this.label = this.options.text || this.options.label;
 
     return this;
   }
@@ -66,40 +92,137 @@ class Button extends Component {
    * @override
    * @return {void}
    */
-  build(props) {
-    super.build(props);
+  build(options) {
+    options = options || this.options;
 
-    var opts = this.options;
-    var type = opts.type;
+    var tag = this.options.tag || 'div';
+    this.element = document.createElement(tag);
 
-    opts.label = opts.label || opts.text;
+    css.add(this.element, this.options.prefix + '-' + this.class);
 
-    //if (opts.text) {
-    this._initLabel(type, opts.icon || opts.name);
-    //}
-
-
-    if (type === null) {
-      type = 'icon-text';
+    if (this.options.css) {
+      css.add(this.element, this.options.css);
     }
 
-    if (opts.name) {
-      this.setAttribute('data-name', opts.name);
-    }
-
-    if (opts.text) {
-      this.setAttribute('title', opts.text);
-    }
-    if (opts.icon) {
-      this._initIcon(type, opts.icon || opts.name);
+    if (this.options.icon) {
+      this.buildIcon(this.options.type, this.options.icon || this.options.name);
     }
 
 
+    if (this.options.name) {
+      console.log('name', this.options.name);
+      this.element.dataset.name = this.options.name;
+    }
 
-    this.setLabel();
+    if (this.label) {
+      this.element.title = this.label;
+    }
 
-    //this._initLabel(type);
-    this.sensor = this.element;
+    this.buildLabel();
+
+    if (this.options.type) {
+      css.add(this.element, 'type-' + this.options.type);
+    }
+
+    if (this.options.style) {
+      var styles = this.options.style.split(/\ /);
+      for (var i = 0; i < styles.length; i++) {
+        css.add(this.element, 'style-' + styles[i]);
+      }
+    }
+
+    if (this.options.color) {
+      css.add(this.element, this.options.color + '-color');
+    }
+
+    if (this.options.content) {
+      this.element.innerHTML = this.options.content;
+    }
+
+    // insert if container options is given
+    if (this.options.container) {
+      //console.log(this.name, opts.container);
+      insert(this.element, this.options.container);
+    }
+
+    return this;
+  }
+
+  /**
+   * [initLabel description]
+   * @return {[type]} [description]
+   */
+  buildLabel(label) {
+    label = label || this.label;
+
+    if (this.options.label === null) return;
+
+    let text = this.options.label || this.options.text;
+
+    this.label = document.createElement('label');
+    css.add(this.label, this.class + '-label');
+    this.label.textContent = text;
+    insert(this.label, this.element);
+  }
+
+  /**
+   * [_initIcon description]
+   * @param  {string} type
+   * @return {string}
+   */
+  buildIcon(type, name) {
+
+    var position = 'top';
+    if (this.options.type === 'text-icon') {
+      position = 'bottom';
+    }
+
+    var tag = 'i';
+    this.icon = document.createElement(tag);
+
+    insert(this.icon, this.element);
+
+    css.add(this.icon, this.class + '-icon');
+
+    css.add(this.element, 'icon-text');
+
+    if (name)
+      css.add(this.icon, name);
+  }
+
+  /**
+   * Setter
+   * @param {string} prop
+   * @param {string} value
+   * @return {Object} The class instance
+   */
+  set(prop, value) {
+
+    switch (prop) {
+      case 'disabled':
+        this.setDisabled(value);
+        break;
+      case 'value':
+        this.setValue(value);
+        break;
+      case 'label':
+        this.setLabel(value);
+        break;
+      default:
+        this.setValue(prop);
+    }
+
+    return this;
+  }
+
+  setDisabled(value) {
+    this.disabled = value;
+
+    if (this.disabled) {
+      css.add(this.element, 'is-disabled');
+    } else {
+      css.remove(this.element, 'is-disabled');
+    }
   }
 
   /**
@@ -117,60 +240,10 @@ class Button extends Component {
     return this;
   }
 
+  insert(container, context) {
+    insert(this.element, container, context);
 
-  /**
-   * [_initIcon description]
-   * @param  {string} type
-   * @return {string}
-   */
-  _initLabel(type, name) {
-
-    var code = name;
-
-    var position = 'top';
-    if (this.options.type === 'text-icon') {
-      position = 'bottom';
-    }
-
-    var prop = {
-      'tag': 'label.ui-text'
-    };
-
-    this.component.label = new Component(prop).insert(this.element, position);
-
-  }
-
-
-  /**
-   * [_initIcon description]
-   * @param  {string} type
-   * @return {string}
-   */
-  _initIcon(type, name) {
-
-    var code = name;
-
-    var position = 'top';
-    if (this.options.type === 'text-icon') {
-      position = 'bottom';
-    }
-
-    var prop = {
-      'tag': 'span.ui-icon'
-    };
-
-    this.icon = new Component(prop).insert(this.element, position);
-
-    // prepare use of svg
-    // this.iconsvg = new Element('svg', prop).insert(this.element);
-    // this.svguse = new Element('use').insert(this.iconsvg);
-
-    // this.iconsvg.setAttribute('viewBox', '0 0 24 24');
-    // this.svguse.setAttribute('xlripple:href','/vendor/icon/content-send.svg');
-
-    if (code) {
-      this.icon.addClass(code);
-    }
+    return this;
   }
 
   /**
@@ -181,8 +254,12 @@ class Button extends Component {
     // e.preventDefault();
     // e.stopPropagation();
 
+    if (this.disabled) return;
+    if (this.options.upload) return;
+
     this.press(e);
   }
+
 
   // /**
   //  * _onMouseDown description
@@ -192,9 +269,8 @@ class Button extends Component {
   //   //console.log('_onMouseDown');
   //   e.preventDefault();
   //   e.stopPropagation();
-  //   this.addClass('is-active');
+  //   css.add(this.element, 'is-active');
   // }
-
 
   /**
    * _onMouseDown description
@@ -205,7 +281,7 @@ class Button extends Component {
     // e.preventDefault();
     // e.stopPropagation();
 
-    this.removeClass('is-active');
+    css.remove(this.element, 'is-active');
   }
 
   /**
@@ -215,7 +291,7 @@ class Button extends Component {
   _onMouseOut(e) {
     e.preventDefault();
     e.stopPropagation();
-    this.removeClass('is-active');
+    css.remove(this.element, 'is-active');
   }
 }
 
