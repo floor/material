f95c8268efb0972e5dc75ac618183c29dad5b886
Jerome Vial <jerome.vial@gmail.com>
2017-09-12 22:05:19 +0200

    add index file

---
+++ index.js
@@ -0,0 +1,41 @@
+import Button from 'material/src/button';
+import Component from 'material/src/component';
+import Card from 'material/src/card';
+import Checkbox from 'material/src/checkbox';
+import Dialog from 'material/src/dialog';
+import Divider from 'material/src/divider';
+import Drawer from 'material/src/drawer';
+import Form from 'material/src/form';
+import Image from 'material/src/image';
+import Item from 'material/src/item';
+import Layout from 'material/src/layout';
+import Layout2 from 'material/src/layout2';
+import List from 'material/src/list';
+import Menu from 'material/src/menu';
+import Slider from 'material/src/slider';
+import Switch from 'material/src/switch';
+import Text from 'material/src/text';
+import Textfield from 'material/src/textfield';
+import Toolbar from 'material/src/toolbar';
+
+export {
+  Button,
+  Component,
+  Card,
+  Checkbox,
+  Dialog,
+  Divider,
+  Drawer,
+  Form,
+  Image,
+  Item,
+  Layout,
+  Layout2,
+  List,
+  Menu,
+  Slider,
+  Switch,
+  Text,
+  Textfield,
+  Toolbar
+};
