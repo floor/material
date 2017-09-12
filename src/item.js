dc8a17f91604f83afb5d7950fe036cb25d7fa5f7
Jerome Vial <jerome.vial@gmail.com>
2017-09-12 22:06:34 +0200

    improve item

--- src/item.js
+++ src/item.js
@@ -1,73 +1,113 @@
 'use strict';
 
-import init from './component/merge';
+//import Component from './component';
 import merge from './module/merge';
-import create from './element/create';
-import events from './component/events';
-import emitter from './module/emitter';
-import bind from './module/bind';
 import insert from './component/insert';
-// element related modules
+import css from './module/css';
+// import bind from '../module/bind';
 
 
 var defaults = {
   prefix: 'material',
   class: 'item',
-  node: null,
-  module: [events, emitter, bind, insert]
+  tag: {
+    default: 'span',
+    display4: 'h1',
+    display3: 'h1',
+    display2: 'h1',
+    display1: 'h1',
+    headline: 'h1',
+    title: 'h2',
+    subheading2: 'h3',
+    subheading1: 'h4',
+    body: 'p',
+    body2: 'aside',
+    caption: 'span'
+  }
 };
 
 /**
- * The item class is used for example as item list
+ * The class represents an item ie for list
  *
  * @class
- * @extends {Component}
  * @return {Object} The class instance
  * @example new Item(object);
  */
-class Item {
+export default class Item {
 
   /**
-   * Constructor
-   * @param  {Object} options - Component options
-   * @return {Object} Class instance 
+   * init
+   * @return {Object} The class options
    */
   constructor(options) {
-    this.options = merge(defaults, options);
-    init(this);
+
+    this.init(options);
     this.build();
 
-    if (this.options.bind) {
-      this.bind(this.options.bind);
-    }
-
     return this;
+  }
+
+  /**
+   * [init description]
+   * @param  {?} options [description]
+   * @return {?}         [description]
+   */
+  init(options) {
+
+    // merge options
+    this.options = merge(defaults, options || {});
+
+    // define class
+
+
+    // assign modules
+    Object.assign(this, insert);
+
   }
 
   /**
    * Build function for item
    * @return {Object} This class instance
    */
+  build(options) {
+    options = options || this.options;
 
-  /**
-   * build the component using the super method
-   * @return {Object} The class instance
-   */
-  build() {
-    var tag = this.options.tag || 'div';
-    this.wrapper = create(tag, this.options.prefix + '-' + this.options.class);
+    // define main tag
+    var tag = options.tag[options.type] || options.tag.default;
 
+    this.wrapper = document.createElement(tag);
+
+    if (options.text) {
+      this.set(options.text);
+    }
+
+    css.add(this.wrapper, this.options.prefix + '-' + this.options.class);
+
+    if (options.type) {
+      css.add(this.wrapper, this.options.class + '-' + options.type);
+    }
+
+    if (this.options.container) {
+      this.insert(this.options.container);
+    }
   }
 
   /**
-   * [focus description]
-   * @return {void}
+   * Get or set text value of the element
+   * @param {string} value The text to set
+   * @returns {*}
    */
   set(value) {
-    this.wrapper.innerHTML(value);
+    if (value) {
+      if (this.wrapper.innerText) {
+        this.wrapper.innerText = value;
+      } else {
+        this.wrapper.textContent = value;
+      }
+
+      return this;
+    }
 
     return this;
   }
-}
-
-export default Item;
+};
