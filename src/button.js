036a4d1cf548b49240c4347263587fe29196d598
Jerome Vial <jerome.vial@gmail.com>
2017-10-01 09:41:48 +0200

    apply fix standard js

--- src/button.js
+++ src/button.js
@@ -1,29 +1,28 @@
-'use strict';
+'use strict'
 
-import init from './component/init';
-import events from './component/events';
-import classify from './component/classify';
-import ripple from './component/ripple';
-import emitter from './module/emitter';
-import create from './element/create';
-import css from './module/css';
-import merge from './module/merge';
-import insert from './element/insert';
-import bind from './module/bind';
+import init from './component/init'
+import classify from './component/classify'
+import ripple from './component/ripple'
+import emitter from './module/emitter'
+import create from './element/create'
+import css from './module/css'
+import merge from './module/merge'
+import insert from './element/insert'
+import bind from './module/bind'
 
 // modules
-import control from './component/control';
+import control from './component/control'
 
 const defaults = {
   prefix: 'material',
   class: 'button',
   tag: 'button',
-  modules: [events, control, emitter, bind, ripple],
+  modules: [control, emitter, bind, ripple],
   build: [],
   bind: {
     'wrapper.click': 'click'
   }
-};
+}
 
 /**
  * Button component
@@ -39,7 +38,6 @@ const defaults = {
  * }).insert(document.body);
  */
 class Button {
-
   /**
    * The init method of the Button class
    * @param  {Object} options [description]
@@ -47,16 +45,16 @@ class Button {
    * @return {Object} The class instance
    */
   constructor(options) {
-    this.options = merge(defaults, options || {});
+    this.options = merge(defaults, options || {})
 
-    this.init();
-    this.build();
-    this.setup();
-    this.bind(this.options.bind);
+    this.init()
+    this.build()
+    this.setup()
+    this.bind(this.options.bind)
 
-    this.emit('ready');
+    this.emit('ready')
 
-    return this;
+    return this
   }
 
   /**
@@ -65,11 +63,11 @@ class Button {
    * @return {?}         [description]
    */
   init() {
-    init(this);
+    init(this)
 
-    this.element = this.element || {};
+    this.element = this.element || {}
 
-    this.emit('init');
+    this.emit('init')
   }
 
   /**
@@ -78,32 +76,31 @@ class Button {
    * @return {void}
    */
   build() {
-    this.element = {};
+    this.element = {}
 
-    var tag = this.options.tag || 'div';
-    this.wrapper = create(tag, this.options.prefix + '-' + this.options.class);
+    var tag = this.options.tag || 'div'
+    this.wrapper = create(tag, this.options.prefix + '-' + this.options.class)
 
-    classify(this.wrapper, this.options);
+    classify(this.wrapper, this.options)
 
-    this.options.label = this.options.label || this.options.text;
+    this.options.label = this.options.label || this.options.text
 
-    this.label(this.options.label);
-    this.icon(this.options.icon);
-
+    this.label(this.options.label)
+    this.icon(this.options.icon)
 
     if (this.options.type) {
-      css.add(this.wrapper, 'type-' + this.options.type);
+      css.add(this.wrapper, 'type-' + this.options.type)
     }
 
     // insert if container options is given
     if (this.options.container) {
-      insert(this.wrapper, this.options.container);
-      this.emit('injected', this.wrapper);
+      insert(this.wrapper, this.options.container)
+      this.emit('injected', this.wrapper)
     }
 
-    this.emit('built', this.wrapper);
+    this.emit('built', this.wrapper)
 
-    return this;
+    return this
   }
 
   /**
@@ -113,9 +110,9 @@ class Button {
    * @return {?}           [description]
    */
   insert(container, context) {
-    insert(this.wrapper, container, context);
+    insert(this.wrapper, container, context)
 
-    return this;
+    return this
   }
 
   /**
@@ -123,27 +120,26 @@ class Button {
    * @return {?} [description]
    */
   setup() {
-
-    this.element.input = this.wrapper;
+    this.element.input = this.wrapper
 
     if (this.options.name) {
-      //console.log('name', this.options.name);
-      this.wrapper.dataset.name = this.options.name;
+      // console.log('name', this.options.name);
+      this.wrapper.dataset.name = this.options.name
     }
 
     if (this.options.label) {
-      this.wrapper.title = this.options.label;
+      this.wrapper.title = this.options.label
     }
 
     if (this.options.style) {
-      var styles = this.options.style.split(/\ /);
+      var styles = this.options.style.split(' ')
       for (var i = 0; i < styles.length; i++) {
-        css.add(this.wrapper, 'style-' + styles[i]);
+        css.add(this.wrapper, 'style-' + styles[i])
       }
     }
 
     if (this.options.content) {
-      this.wrapper.innerHTML = this.options.content;
+      this.wrapper.innerHTML = this.options.content
     }
   }
 
@@ -154,22 +150,21 @@ class Button {
    * @return {Object} The class instance
    */
   set(prop, value) {
-
     switch (prop) {
       case 'disabled':
-        this.disable(value);
-        break;
+        this.disable(value)
+        break
       case 'value':
-        this.setValue(value);
-        break;
+        this.setValue(value)
+        break
       case 'label':
-        this.setLabel(value);
-        break;
+        this.setLabel(value)
+        break
       default:
-        this.setValue(prop);
+        this.setValue(prop)
     }
 
-    return this;
+    return this
   }
 
   /**
@@ -178,16 +173,16 @@ class Button {
    * @return {void}
    */
   click(e) {
-    e.preventDefault();
+    e.preventDefault()
 
-    if (this.disabled === true) return;
-    if (this.options.upload) return;
+    if (this.disabled === true) return
+    if (this.options.upload) return
 
-    //this.publish('click');
-    this.emit('press', e);
+    // this.publish('click');
+    this.emit('press', e)
 
-    return this;
+    return this
   }
 }
 
-export default Button;
+export default Button
