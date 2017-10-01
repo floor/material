717e2a7aa649878a079d80608c6f018561306c87
Jerome Vial <jerome.vial@gmail.com>
2017-10-01 09:27:16 +0200

    apply fix standard js on index file

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-import Button from 'material/src/button';
-import Calendar from 'material/src/calendar';
-import Card from 'material/src/card';
-import Component from 'material/src/component';
-import Container from 'material/src/container';
-import Checkbox from 'material/src/checkbox';
-import Dialog from 'material/src/dialog';
-import Divider from 'material/src/divider';
-import Drawer from 'material/src/drawer';
-import Form from 'material/src/form';
-import Image from 'material/src/image';
-import Item from 'material/src/item';
-import Layout from 'material/src/layout';
-import Layout2 from 'material/src/layout2';
-import List from 'material/src/list';
-import Menu from 'material/src/menu';
-import Slider from 'material/src/slider';
-import Switch from 'material/src/switch';
-import Text from 'material/src/text';
-import Textfield from 'material/src/textfield';
-import Toolbar from 'material/src/toolbar';
+import Button from 'material/src/button'
+import Calendar from 'material/src/calendar'
+import Card from 'material/src/card'
+import Component from 'material/src/component'
+import Container from 'material/src/container'
+import Checkbox from 'material/src/checkbox'
+import Dialog from 'material/src/dialog'
+import Divider from 'material/src/divider'
+import Drawer from 'material/src/drawer'
+import Form from 'material/src/form'
+import Image from 'material/src/image'
+import Item from 'material/src/item'
+import Layout from 'material/src/layout'
+import Layout2 from 'material/src/layout2'
+import List from 'material/src/list'
+import Menu from 'material/src/menu'
+import Slider from 'material/src/slider'
+import Switch from 'material/src/switch'
+import Text from 'material/src/text'
+import Textfield from 'material/src/textfield'
+import Toolbar from 'material/src/toolbar'
 
 export {
   Button,
@@ -42,4 +42,4 @@ export {
   Text,
   Textfield,
   Toolbar
-};
+}
