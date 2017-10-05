85af2f0bf2f9d6d88d02748744b3b9fc9e354f86
Jerome Vial <jerome.vial@gmail.com>
2017-10-05 17:30:47 +0200

    update upgraded components

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
+import build from 'material/src/component/build'
 import button from 'material/src/button'
 import Calendar from 'material/src/calendar'
 import Card from 'material/src/card'
-import Component from 'material/src/component'
-import Container from 'material/src/container'
+import component from 'material/src/component'
+import container from 'material/src/container'
 import Checkbox from 'material/src/checkbox'
 import Dialog from 'material/src/dialog'
-import Divider from 'material/src/divider'
+import divider from 'material/src/divider'
 import Drawer from 'material/src/drawer'
 import Form from 'material/src/form'
 import Image from 'material/src/image'
-import Item from 'material/src/item'
+import item from 'material/src/item'
 import Layout from 'material/src/layout'
 import List from 'material/src/list'
 import Menu from 'material/src/menu'
@@ -20,18 +21,19 @@ import Textfield from 'material/src/textfield'
 import Toolbar from 'material/src/toolbar'
 
 export {
+  build,
   button,
   Calendar,
   Card,
   Checkbox,
-  Component,
-  Container,
+  component,
+  container,
   Dialog,
-  Divider,
+  divider,
   Drawer,
   Form,
   Image,
-  Item,
+  item,
   Layout,
   List,
   Menu,
