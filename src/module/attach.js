40654cb854dc111e7673e408dc89e58034fd4b0a
Jerome Vial <jerome.vial@gmail.com>
2019-05-07 02:14:00 +0200

    enable on emitter

--- src/module/attach.js
+++ src/module/attach.js
@@ -16,20 +16,54 @@ import extract from './extract'
  */
 export default {
   attach: function (events) {
+    // console.log('attach', events)
     events = events || this.options.events
     if (!events) return
 
     // console.log('attach', events, this)
     var instance = this
     events.map((def) => {
-      // console.log('map', def)
-
       var e = extract.e(instance, def[0])
       var f = extract.f(instance, def[1])
+      var o = def[2] || false
 
-      e.element.addEventListener(e.name, f.bind(this))
+      // console.log('attach', def[0], def[1])
+
+      var keys = def[1].split('.')
+      keys.pop()
+      var bound = this._path(keys.join('.'))
+
+      if (e.element && e.element.addEventListener) {
+        e.element.addEventListener(e.name, f.bind(bound), o)
+      } else if (e.element && e.element.on) {
+        // console.log('on', e.name)
+        e.element.on(e.name, f.bind(bound))
+      } else {
+        console.log('can\'t attach', e.name)
+      }
     })
 
     return this
+  },
+
+  /**
+   * Return the last reference from an object
+   * @param  {string} str Object path for example key1.key2.key3
+   * @return {value} The value of the last reference
+   */
+  _path: function (str) {
+    if (!str) return this
+    else if (!str.match(/\./)) return this[str]
+    var last
+
+    var keys = str.split('.')
+    for (var i = 0, l = keys.length; i < l; i++) {
+      var key = keys[i]
+
+      last = last || this
+      last = last[key]
+    }
+
+    return last
   }
 }
