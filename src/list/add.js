35a19b354348452ba144ce1b3de6a135992ad6cb
Jerome Vial <jerome.vial@gmail.com>
2019-11-30 18:08:10 +0100

    added add method for list

--- src/list/add.js
+++ src/list/add.js
@@ -1,4 +1,20 @@
 
 export default {
+  add (info, context) {
+    // console.log('data add', this.dataStore[info._id])
+    context = context || null
 
+    if (this.dataStore[info._id]) {
+      // console.log('already in the list')
+      return
+    }
+
+    if (context === 'top') {
+      this.data.unshift(info)
+    } else {
+      this.data.push(info)
+    }
+    // console.log('data', this.data.length, this.data)
+    this.render(this.data)
+  }
 }
