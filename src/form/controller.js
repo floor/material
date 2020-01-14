9f4166f71f729a76e82d54161848013e96d194eb
Jerome Vial <jerome.vial@gmail.com>
2020-01-14 08:35:06 +0100

    add mode to controller and remove mode

--- src/form/controller.js
+++ src/form/controller.js
@@ -19,6 +19,25 @@ export default {
     }
   },
 
+  setMode (mode) {
+    // console.log('setMode', mode)
+    this.mode = mode
+
+    this.root.classList.add('mode-' + mode)
+
+    this.emit('mode', mode)
+  },
+
+  changeMode (mode) {
+    if (mode === 'update' && this.enableControls) {
+      this.enableControls()
+    } else if (mode === 'update' && this.disableControls) {
+      this.disableControls()
+    } else if (mode === 'read' && this.disableControls) {
+      this.disableControls()
+    }
+  },
+
   bindControl (control) {
     if (control && control.on) {
       control.on('change', () => {
