34e7b6048dc996bd1d80bfbd907495f7cccce328
Jerome Vial <jerome.vial@gmail.com>
2020-01-14 08:56:02 +0100

    enhance update method by update data list

--- src/list/update.js
+++ src/list/update.js
@@ -11,10 +11,17 @@ export default {
     // console.log('item', item)
 
     item.innerHTML = ''
-
     var layout = new Layout(this.options.layout.item, item)
 
     this.renderInfo(layout, info)
+
+    this.dataStore[info._id] = info
+
+    for (var i = 0; i < this.data.length; i++) {
+      if (this.data[i]._id === info._id) {
+        this.data[i] = info
+      }
+    }
 
     return item
   }
