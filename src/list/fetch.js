3eafc50bea2641a4936f645eacbd467aaffdd8a7
Jerome Vial <jerome.vial@gmail.com>
2020-02-01 00:26:14 +0100

    enhance fetch

--- src/list/fetch.js
+++ src/list/fetch.js
@@ -1,30 +1,32 @@
 export default {
   fetch (page, size) {
-    if (this.controller) {
-      this.controller.abort()
+    this.ui.body.innerHTML = ''
+
+    if (this.abortController) {
+      this.abortController.abort()
     }
 
-    this.controller = new AbortController()
-    var signal = this.controller.signal
+    this.abortController = new AbortController()
+    var signal = this.abortController.signal
 
     page = page || 1
     size = size || this.options.list.size
     // console.log('fetch')
 
-    var route = this.buildRoute(page, size)
+    if (page === 1) {
+      this.ui.body.scrolltop = 0
+    }
 
-    this.ui.body.innerHTML = ''
-    // console.log('route', route)
+    var route = this.buildRoute(page, size)
 
     fetch(route, {signal}).then((resp) => {
       return resp.json()
     }).then((data) => {
       // console.log('data', route, data)
-
+      this.data = []
       this.storeData(data)
 
       this.data = data
-
       this.render(data)
 
       this.emit('fetched', data)
@@ -33,37 +35,58 @@ export default {
     })
   },
 
+  addParams (route, params) {
+    // console.log('addParams', route, params)
+
+    if (!route) {
+      console.log('error: no route specified!')
+
+      return
+    }
+
+    if (route.indexOf('?') > -1) {
+      // console.log('?', route, route.length, route[route.length - 1])
+      if (route[route.length - 1] === '&' || route[route.length - 1] === '?') {
+        route = route + params
+      } else {
+        route = route + '&' + params
+      }
+    } else {
+      // console.log('add ?')
+      route = route + '?' + params
+    }
+
+    return route
+  },
+
   buildRoute (page, size, path) {
+    // console.log('buildroute', this.options.route, path)
     path = path || 'list'
 
     page = page || 1
     size = size || this.options.list.size
-    var params = ''
 
-    var route = this.options.route[path]
+    var route = this.options.route
+
+    if (path) {
+      route = this.options.route[path]
+    }
+
+    if (this.routeAddOn) {
+      route = this.routeAddOn(route)
+    }
 
     if (this.params) {
-      params = this.params() || ''
+      // console.log('params', route, this.params())
+      route = this.addParams(route, this.params())
     }
 
-    if (route.indexOf('?') > -1) {
-      route = route + params
-    } else {
-      route = route + '?' + params
-    }
-
-    if (page === 1) {
-      this.ui.body.scrolltop = 0
+    if (this.ui.filter && this.ui.filter.root.classList.contains('selected')) {
+      route = this.addParams(route, this.getFilter())
     }
 
     if (this.options.pagination) {
-      // console.log('pagination', route.indexOf('?'), route)
-
-      if (route.indexOf('?') > -1) {
-        route = route + '&page=' + page + '&size=' + size
-      } else {
-        route = route + '?page=' + page + '&size=' + size
-      }
+      route = this.addParams(route, 'page=' + page + '&size=' + size)
     }
 
     return route
