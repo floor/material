c21a2b99d7fccd7054e77dd2b9a0e67c3087f4a2
Jerome Vial <jerome.vial@gmail.com>
2020-02-10 22:56:39 +0100

    move some methods from submit to data

--- src/form/submit.js
+++ src/form/submit.js
@@ -1,31 +1,12 @@
-import { byString } from '../module/object'
-
 export default {
 
   submit (ev) {
     ev.preventDefault()
     // console.log('submit', this.mode)
-    // ev.preventDefault()
-    if (this.verify && !this.verify()) return
 
-    // console.log('submit')
-    if (this.disableControls) {
-      this.disableControls()
-    }
+    var data = this.initData()
 
-    var data = new FormData()
-
-    if (this.info && this.info._id) {
-      // console.log('id', this.info._id)
-      data.append('id', this.info._id)
-    }
-
-    if (this.info && this.info.uuid) {
-      data.append('uuid', this.info.uuid)
-    }
-
-    this.appendFields(data)
-    this.appendFiles(data)
+    console.log('data', data)
 
     if (this.update) {
       this.update(data)
@@ -55,51 +36,6 @@ export default {
         method: method,
         formData: formData
       })
-    }
-  },
-
-  appendFields (formData) {
-    for (var field in this.field) {
-      if (this.field.hasOwnProperty(field)) {
-        // console.log('check field', field)
-        var value = this.field[field].get()
-        if (value !== null) {
-          if (this.mode === 'update' && this.options.update && this.options.update.modifiedOnly) {
-            var modified = this.isModified(field, value)
-            if (modified) {
-              formData.append(field, value)
-            }
-          } else {
-            formData.append(field, value)
-          }
-        }
-      }
-    }
-  },
-
-  isModified (field, value) {
-    var initial = byString(this.info, field)
-
-    if (typeof initial === 'number') {
-      value = Number(value)
-    }
-
-    if (initial !== value) {
-      // console.log('field', field, typeof value, value)
-      return true
-    } else {
-      return false
-    }
-  },
-
-  appendFiles (formData) {
-    // console.log('appendFiles', this.file)
-    for (var file in this.file) {
-      // console.log('this file', this.file, this.file[file].input)
-      // console.log('file', file, this.file[file])
-      if (this.file.hasOwnProperty(file) && this.file[file].input && this.file[file].input.value) {
-        formData.append(file, this.file[file].input.files[0])
-      }
     }
   }
 }
