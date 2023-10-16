fc958ed3e64ff1e0e8c51f306e8e1348990bb32f
Jerome Vial <jerome.vial@gmail.com>
2023-10-16 19:44:56 +0200

    Use passive event if available

--- src/module/attach.js
+++ src/module/attach.js
@@ -6,19 +6,11 @@ import extract from './extract'
  * @category module
  */
 
-/**
- * [attach description]
- * @param  {object} component [description]
- * @param  {[type]} events    [description]
- * @return {[type]}           [description]
- */
 export default {
   attach: function (events) {
-    // console.log('attach', events)
     events = events || this.options.events
     if (!events) return
 
-    // console.log('attach', events, this)
     var instance = this
     events.map((def) => {
       var e = extract.e(instance, def[0])
@@ -26,18 +18,16 @@ export default {
       var option = def[2]
 
       var keys = def[1].split('.')
-
       keys.pop()
       var bound = this.last(keys.join('.'))
 
       if (f && bound && e && e.element && e.element.addEventListener) {
         if (!f) { console.log('error') }
 
-        if (option) {
-          e.element.addEventListener(e.name, f.bind(bound), option)
-        } else {
-          e.element.addEventListener(e.name, f.bind(bound))
-        }
+        // Use passive: true for passive event listeners
+        var eventOptions = option || { passive: true }
+
+        e.element.addEventListener(e.name, f.bind(bound), eventOptions)
       } else if (e && e.element && e.element.on && f && bound) {
         e.element.on(e.name, f.bind(bound))
       } else {
@@ -55,17 +45,14 @@ export default {
    * @return {value} The value of the last reference
    */
   last: function (str) {
-    // console.log('_path', str)
     if (!str) return this
-    else if (!str.match(/\./)) return this[str]
-    var last
 
-    var keys = str.split('.')
-    for (var i = 0, l = keys.length; i < l; i++) {
-      var key = keys[i]
+    const keys = str.split('.')
+    let last = this
 
-      last = last || this
+    for (const key of keys) {
       last = last[key]
+      if (last === undefined) return undefined
     }
 
     return last
