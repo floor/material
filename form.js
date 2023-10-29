e424c072a69ece1f9b07bb114a9816e5f3b32f63
Jerome Vial <jerome.vial@gmail.com>
2023-10-29 17:36:47 +0100

    Add index file for list and form

---
+++ form.js
@@ -0,0 +1,27 @@
+import build from './src/form/build'
+import cancel from './src/form/cancel'
+import controller from './src/form/controller'
+import create from './src/form/create'
+import data from './src/form/data'
+import del from './src/form/delete'
+import fetch from './src/form/fetch'
+import render from './src/form/render'
+import set from './src/form/set'
+import submit from './src/form/submit'
+import update from './src/form/update'
+import verify from './src/form/verify'
+
+export {
+  build,
+  cancel,
+  controller,
+  create,
+  data,
+  del,
+  fetch,
+  render,
+  set,
+  submit,
+  update,
+  verify
+}
---
+++ list.js
@@ -0,0 +1,35 @@
+import add from './src/list/add'
+import build from './src/list/build'
+import click from './src/list/click'
+import create from './src/list/create'
+import display from './src/list/display'
+import empty from './src/list/empty'
+import filter from './src/list/filter'
+import image from './src/list/image'
+import load from './src/list/load'
+import remove from './src/list/remove'
+import render from './src/list/render'
+import request from './src/list/request'
+import search from './src/list/search'
+import select from './src/list/select'
+import status from './src/list/status'
+import update from './src/list/update'
+
+export {
+  add,
+  build,
+  click,
+  create,
+  display,
+  empty,
+  filter,
+  image,
+  load,
+  remove,
+  render,
+  request,
+  search,
+  select,
+  status,
+  update
+}
