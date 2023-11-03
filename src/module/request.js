d6186c972236ffd18557a1349d34e76149d0d944
Jerome Vial <jerome.vial@gmail.com>
2023-11-03 17:46:45 +0100

    Add request module

---
+++ src/module/request.js
@@ -0,0 +1,33 @@
+// request.js
+const request = async (url, method = 'GET', body = null, headers = {}, signal = null) => {
+  const defaultHeaders = {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  }
+
+  const options = {
+    method,
+    headers: { ...defaultHeaders, ...headers },
+    signal
+  }
+
+  if (body) {
+    options.body = JSON.stringify(body)
+  }
+
+  try {
+    const response = await fetch(url, options)
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
+    const data = await response.json()
+
+    return data
+  } catch (error) {
+    // Handle fetch or data processing errors
+    // console.log('error', error)
+    return { error }
+  }
+}
+
+export default request
